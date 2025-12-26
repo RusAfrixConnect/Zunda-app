b1274750c07dd5669cef683ebaec413dfd2e58b9
RusAfrixConnect <dabaran75@gmail.com>
2025-12-26 21:13:06 +0300

    Create App.test.js

---
+++ App.test.js
@@ -0,0 +1,11 @@
+// App.test.js - Test minimal
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>TEST SANS ERREUR</Text>
+    </View>
+  );
+}
